2b47ca30a8ea3029cf3a2de091973eef8e933be6
Santi Hnatiuk <59941685+PedritusMaximus@users.noreply.github.com>
2024-05-23 19:50:17 -0300

    Web Scraping de archivos csv
    Se agregaron las funciones para acceder a la url de BA data sets (https://data.buenosaires.gob.ar/dataset/flujo-vehicular-por-radares-ausa) y descargar (según el año) el dataset de flujo vehicular. El archivo se descarga en la carpeta donde se esta ejecutando el main.py

--- TrafficFlow/main.py
+++ TrafficFlow/main.py
@@ -28,12 +28,51 @@ from sympy import*
 get_ipython().run_line_magic('matplotlib', 'qt5') 
 
 #%%
+import requests
+from bs4 import BeautifulSoup
+ 
+def status_url(url): # if res.ok < 400, status = True
+    res = requests.get(url)
+    status = res.ok
+    return status
+
+def get_href(url):
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    soups = soup.findAll('a', class_ = 'btn')
+    for s in soups :
+        if s.get('href')[-8:-4] == year:
+            return s.get('href')
+            
+def save_in_folder(url):
+    r = requests.get(url)
+    file = 'traffic_flow' + year + '.csv'
+    with open(file,'wb') as f:
+        f.write(r.content)
+    return file   
+if __name__ == '__main__':
+    url = 'https://data.buenosaires.gob.ar/dataset/flujo-vehicular-por-radares-ausa'; year = '2024'
+    if status_url(url) == True:
+        link = get_href(url)
+        file = save_in_folder(link)
+    else:
+        print(False)
+#%%
 
 df = pd.read_csv('dataset_flujo_vehicular.csv')
-df= df.drop_duplicates(keep='first')
-
+df = df.drop_duplicates(keep='first')
+df = df.dropna(how='any')    #to drop if any value in the row has a nan
 import datetime as dt
 df['HORA'] = df['HORA'].apply(lambda x: dt.datetime.strptime(x,'%d%b%Y:%H:%M:%S')) # Convierto la fecha en formato dd-mm-aaaa...
- 
 
-    
+detectors = df.drop_duplicates(subset= ['CODIGO_LOCACION'],keep = 'last')
+print(detectors)
+
+#%%
+
+df2 = pd.read_csv(file)
+df2 = df2.drop_duplicates(keep='first')
+df2 = df2.dropna(how='any') 
+
+detectors2 = df2.drop_duplicates(subset= ['Disp Nombre'],keep = 'first')
+print(detectors2)
+
